1e39bf3c75b22c85ff4041e67d398188c942f2ff
Sumalee SANGUANCHOM <sumalee.s@essilor.co.th>
2025-08-26 14:06:07 +0700

    update ingest

--- notebooks/01_bronze_layer/01_ingest.py
+++ notebooks/01_bronze_layer/01_ingest.py
@@ -1,77 +1,105 @@
 # Databricks notebook source
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, TimestampType, DateType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, TimestampType, DateType, BooleanType, ArrayType
+
 # COMMAND ----------
+
 # define variable
 catalog_name = "workspace"
 schema_name = "	default"
+input_path = "/Volumes/workspace/default/data/input"
 
 # COMMAND ----------
+
 # Define explicit schemas for enforcement
 claim_schema = StructType([
     StructField("ClaimID", StringType(), True),
-    StructField("PatientID", StringType(), True),
+    StructField("MemberID", StringType(), True),
     StructField("ProviderID", StringType(), True),
-    StructField("DiagnosisCode", StringType(), True),
-    StructField("ProcedureCode", StringType(), True),
-    StructField("ClaimAmount", DoubleType(), True),
     StructField("ClaimDate", DateType(), True),
+    StructField("ServiceDate", DateType(), True),
+    StructField("Amount", DoubleType(), True),
     StructField("Status", StringType(), True),
+    StructField("ICD10Codes", StringType(), True),
+    StructField("CPTCodes", StringType(), True),
+    StructField("ClaimType", StringType(), True),
+    StructField("SubmissionChannel", StringType(), True),
+    StructField("Notes", StringType(), True),
+    StructField("IngestTimestamp", TimestampType(), True),
 ])
 
 diagnosis_schema = StructType([
-    StructField("DiagnosisCode", StringType(), True),
+    StructField("Code", StringType(), True),
     StructField("Description", StringType(), True),
 ])
 
-patient_schema = StructType([
-    StructField("PatientID", StringType(), True),
-    StructField("PatientName", StringType(), True),
+member_schema = StructType([
+    StructField("MemberID", StringType(), True),
+    StructField("Name", StringType(), True),
     StructField("DOB", DateType(), True),
     StructField("Gender", StringType(), True),
-    StructField("Address", StringType(), True),
+    StructField("Region", StringType(), True),
+    StructField("PlanType", StringType(), True),
+    StructField("EffectiveDate", DateType(), True),
+    StructField("Email", StringType(), True),
+    StructField("IsActive", BooleanType(), True),
+    StructField("LastUpdated", DateType(), True),
 ])
-
 
 provider_schema = StructType([
     StructField("ProviderID", StringType(), True),
-    StructField("ProviderName", StringType(), True),
-    StructField("Specialty", StringType(), True),
-    StructField("Location", StringType(), True),
+    StructField("Name", StringType(), True),
+    StructField("Specialties", ArrayType(StringType()), True),
+    StructField("Locations", ArrayType(
+        StructType([
+            StructField("Address", StringType(), True),
+            StructField("City", StringType(), True),
+            StructField("State", StringType(), True)
+        ])
+    ), True),
+    StructField("IsActive", BooleanType(), True),
+    StructField("TIN", StringType(), True),
+    StructField("LastVerified", DateType(), True)
 ])
+
 
 # COMMAND ----------
 
-input_path = "/Volumes/workspace/default/data/input"
+
 
 claim_df = (spark.read
             .format("csv")
             .schema(claim_schema)
             .option("header", "true")
             .option("DateFormat", "yyyy-MM-dd")
-            .load(f"{input_path}/claims.csv"))
+            .option("TimestampFormat", "yyyy-MM-dd hh:mm:ss")
+            .load(f"{input_path}/claims_batch.csv"))
 
 diagnosis_df = (spark.read
                .format("csv")
                .schema(diagnosis_schema)
                .option("header", "true")
-               .load(f"{input_path}/diagnosis_codes.csv"))
+               .load(f"{input_path}/diagnosis_ref.csv"))
 
-patient_df = (spark.read
+member_df = (spark.read
                 .format("csv")
-                .schema(patient_schema)
+                .schema(member_schema)
                 .option("header", "true")
                 .option("DateFormat", "yyyy-MM-dd")
-                .load(f"{input_path}/patients.csv"))
+                .load(f"{input_path}/members.csv"))
 
 provider_df = (spark.read
-                .format("csv")
+                .format("json")
                 .schema(provider_schema)
                 .option("header", "true")
                 .option("DateFormat", "yyyy-MM-dd")
-                .load(f"{input_path}/providers.csv"))
+                .load(f"{input_path}/providers.json"))
 
 
-(claim_df.write.format("delta").mode("overwrite").saveAs(f"{catalog_name}.{schema_name}.bt_claim"))
-(diagnosis_df.write.format("delta").mode("overwrite").save(f"{catalog_name}.{schema_name}.bt_diagnosis"))
-(patient_df.write.format("delta").mode("overwrite").save(f"{catalog_name}.{schema_name}.bt_patient"))
-(provider_df.write.format("delta").mode("overwrite").save(f"{catalog_name}.{schema_name}.bt_provider"))
+
+
+# COMMAND ----------
+
+(claim_df.write.format("delta").mode("overwrite").saveAsTable(f"{catalog_name}.{schema_name}.bt_claim"))
+(diagnosis_df.write.format("delta").mode("overwrite").saveAsTable(f"{catalog_name}.{schema_name}.bt_diagnosis"))
+(member_df.write.format("delta").mode("overwrite").saveAsTable(f"{catalog_name}.{schema_name}.bt_member"))
+(provider_df.write.format("delta").mode("overwrite").saveAsTable(f"{catalog_name}.{schema_name}.bt_provider"))
